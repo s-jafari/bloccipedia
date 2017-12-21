d0e49fb04b189b6f76a7d311ee8ef2c6d0a1f646
Shervin Jafari <shv.jafari@gmail.com>
2017-12-21 23:44:16 +0100

    current state with error

---
+++ app/controllers/wiki_controller.rb
@@ -0,0 +1,55 @@
+class WikiController < ApplicationController
+  def index
+    @wikis = Wiki.all
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wikis.build(wiki_params)
+    @wiki.user = current_user
+
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to [@wiki]
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.assign_attributes(wiki_params)
+
+    if @wiki.save
+      flash[:notice] = "Wiki was updated."
+      redirect_to [@wiki]
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to :index
+    else
+      flash.now[:alert] = "There was an error deleting the wiki."
+      render :show
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :wikis
+
   devise_for :views
   devise_for :users
-  get 'welcome/index'
 
   root 'welcome#index'
 end
