db6bca645eb6671f1d4d74b5d0c21f2036342290
Shervin Jafari <shv.jafari@gmail.com>
2018-01-12 13:18:23 +0000

    adding markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def markdown(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true
+    }
+
+    extensions = {
+      autolink:           true,
+      underline:          true,
+      strikethrough:      true,
+      space_after_headers: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
 end
